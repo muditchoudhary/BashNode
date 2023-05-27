66f8d3508071c1b029d28247d3ec4f18eb3fbe6f
Mudit Choudhary <cmudit60@gmail.com>
2023-05-27 09:42:40 +0530

    Finish styling the tagline section

---
+++ client/bashnode/src/Components/Tagline.jsx
@@ -0,0 +1,13 @@
+const Tagline = () => {
+	return (
+		<>
+			<div data-testid="tagline-container" className=" flex flex-col items-center font-bold text-sm my-2 ">
+				<p>&quot;Unleash Your Words, Inspire The World&quot;</p>
+				<p>With</p>
+				<p>BashNode</p>
+			</div>
+		</>
+	);
+};
+
+export default Tagline;
