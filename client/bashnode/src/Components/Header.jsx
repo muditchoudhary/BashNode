50fbbb201e68dd6d861bb56b02739a70079b0b93
Mudit Choudhary <cmudit60@gmail.com>
2023-05-27 09:28:38 +0530

    Finished styling the header section

--- client/bashnode/src/Components/Header.jsx
+++ client/bashnode/src/Components/Header.jsx
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
-import bashNodeLogo from "../assets/react.svg";
+import bashNodeLogo from "../assets/images/hashnode-icon.svg";
 
 const CompanyName = (props) => {
-	return <p className="text-3xl font-bold underline">{props.name}</p>;
+	return <p className=" text-electric-blue font-bold text-2xl">{props.name}</p>;
 };
 CompanyName.propTypes = {
 	name: PropTypes.string,
 };
 
 const CompanyLogo = (props) => {
-	return <img src={props.logo} alt={props.altLogoText} />;
+	return <img src={props.logo} alt={props.altLogoText} className=" w-7 h-7 " />;
 };
 CompanyLogo.propTypes = {
 	logo: PropTypes.string,
@@ -19,7 +19,7 @@ CompanyLogo.propTypes = {
 const Header = () => {
 	return (
 		<>
-			<div>
+			<div className="header flex items-center gap-2 py-2 px-1 border-b-2 border-gray-700 " data-testid="header">
 				<CompanyLogo
 					logo={bashNodeLogo}
 					altLogoText={"BashNode's Logo"}
