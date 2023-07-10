f93fabff1c91e62acdee9bffe4ab190890bc3f12
Mudit Choudhary <cmudit60@gmail.com>
2023-07-10 14:24:43 +0530

    refactor: moved the routing code from main.jsx to App.jsx

---
+++ client/bashnode/src/App.jsx
@@ -0,0 +1,48 @@
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+
+import "./index.css";
+
+import UserAuthentication from "./layout/UserAuthentication";
+import SignUpForm from "./Components/SignUpForm";
+import SignInForm from "./Components/SignInForm";
+import publishArticleImg from "./assets/images/publish-article.svg";
+
+import RootLayout from "./layout/RootLayout";
+
+//CONSTANT VARIABLES
+const FOOTER_TEXT = {
+	signUp: "Sign In to your existing account",
+	signIn: "Sign Up to create a new account",
+};
+const LINK_TO = { signUp: "/auth/sign-in", signIn: "/auth/sign-up" };
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<h1>Home Page</h1>} />
+			<Route
+				path="/auth"
+				element={
+					<UserAuthentication
+						footerText={FOOTER_TEXT}
+						linkTo={LINK_TO}
+						img={publishArticleImg}
+					/>
+				}
+				errorElement={<h2>error from auth</h2>}
+			>
+				<Route path="sign-up" element={<SignUpForm />} />
+				<Route path="sign-in" element={<SignInForm />} />
+			</Route>
+		</Route>
+	)
+);
+
+export const App = () => {
+	return <RouterProvider router={router} />;
+};
--- client/bashnode/src/main.jsx
+++ client/bashnode/src/main.jsx
@@ -1,47 +1,12 @@
+import { App } from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from "react-router-dom";
-
-import "./index.css";
-import UserAuthentication from "./layout/UserAuthentication";
-import SignUpForm from "./Components/SignUpForm";
-import SignInForm from "./Components/SignInForm";
-import publishArticleImg from "./assets/images/publish-article.svg";
-
-//CONSTANT VARIABLES
-const FOOTER_TEXT = {
-	signUp: "Sign In to your existing account",
-	signIn: "Sign Up to create a new account",
-};
-const LINK_TO = { signUp: "/auth/sign-in", signIn: "/auth/sign-up" };
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route
-			path="/auth"
-			element={
-				<UserAuthentication
-					footerText={FOOTER_TEXT}
-					linkTo={LINK_TO}
-					img={publishArticleImg}
-				/>
-			}
-			errorElement={<p>hello error</p>}
-		>
-			<Route path="sign-up" element={<SignUpForm />} />
-			<Route path="sign-in" element={<SignInForm />} />
-
-			<Route path="*" element={<p>hello error 1</p>} />
-		</Route>
-	)
-);
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<AuthContextProvider>
+			<App />
+		</AuthContextProvider>
 	</React.StrictMode>
 );
