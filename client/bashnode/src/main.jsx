f3b99f4a1949c682085eaaf118c55c705d97670d
Mudit Choudhary <cmudit60@gmail.com>
2023-08-16 07:09:59 +0530

    style: Add a add design global config provider to have consistant UI

--- client/bashnode/src/main.jsx
+++ client/bashnode/src/main.jsx
@@ -1,12 +1,22 @@
-import { App } from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AuthContextProvider } from "./context/AuthContext";
+import { ConfigProvider } from "antd";
 
+import { App } from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<AuthContextProvider>
-			<App />
-		</AuthContextProvider>
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: "#2563eb",
+					colorInfo: "#2563eb",
+				},
+			}}
+		>
+			<AuthContextProvider>
+				<App />
+			</AuthContextProvider>
+		</ConfigProvider>
 	</React.StrictMode>
 );
