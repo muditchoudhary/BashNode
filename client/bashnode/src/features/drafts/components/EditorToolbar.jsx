4caa73f3b27b7922d1834ef7c27090c1cbc9b9f2
Mudit Choudhary <cmudit60@gmail.com>
2023-08-17 13:07:20 +0530

    design: add a save button in editor toolbar

--- client/bashnode/src/features/drafts/components/EditorToolbar.jsx
+++ client/bashnode/src/features/drafts/components/EditorToolbar.jsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Space } from "antd";
 import Icon from "@ant-design/icons";
 import PropTypes from "prop-types";
 
@@ -6,13 +6,18 @@ import { ReactComponent as DrawerIcon } from "../../../assets/icons/sidebar-draw
 
 export const EditorToolbar = ({ showDrawer }) => {
 	return (
-        // border-2 border-red-800 border-solid p-2
+		// border-2 border-red-800 border-solid p-2
 		<div className="editor-toolbar flex justify-between p-3 ">
-			<Icon
-				component={DrawerIcon}
-				onClick={showDrawer}
-			/>
-			<Button type="primary" shape="round">Publish</Button>
+			<Icon component={DrawerIcon} onClick={showDrawer} />
+
+			<Space>
+				<Button type="primary" shape="round">
+					Save
+				</Button>
+				<Button type="primary" shape="round">
+					Publish
+				</Button>
+			</Space>
 		</div>
 	);
 };
