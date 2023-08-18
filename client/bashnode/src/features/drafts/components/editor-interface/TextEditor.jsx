03c209711a1e0afc08fdb445fe7b2a3a3bff63ca
Mudit Choudhary <cmudit60@gmail.com>
2023-08-18 14:47:30 +0530

    forgot adding this file with previous commit: commit 2ca7188f75cfb3ba99c9c26babad5160a5755e15

--- client/bashnode/src/features/drafts/components/editor-interface/TextEditor.jsx
+++ client/bashnode/src/features/drafts/components/editor-interface/TextEditor.jsx
@@ -1,27 +1,55 @@
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
+
+const TITLE_VALIDATION = {
+	required: {
+		value: true,
+		message: "required",
+	},
+	maxLength: {
+		value: 300,
+		message: "is too long (maximum is 300 characters)",
+	},
+	minLength: {
+		value: 20,
+		message: "is too short (minimum is 20 characters)",
+	},
+};
+const ARTICLE_VALIDATION = {
+	required: {
+		value: true,
+		message: "required",
+	},
+	maxLength: {
+		value: 20000,
+		message: "is too long (maximum is 20000 characters)",
+	},
+	minLength: {
+		value: 20,
+		message: "is too short (minimum is 20 characters)",
+	},
+};
 
 export const TextEditor = () => {
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-	} = useForm();
-
-	const onSubmit = (data) => console.log(data);
-
-	console.log(watch("title"));
+	const { register } = useFormContext();
 	return (
 		// border-purple border-solid
 		<form
-			onSubmit={handleSubmit(onSubmit)}
+			onSubmit={(e) => e.preventDefault()}
 			className="border-2 flex flex-col flex-1 "
 		>
-			<input defaultValue={"test"} {...register("title")} className="flex-1" />
+			<input
+				defaultValue={
+					"Hello world this is the test title and it is long"
+				}
+				{...register("title", { ...TITLE_VALIDATION })}
+				className="flex-1"
+			/>
 			<textarea
-				defaultValue={"text textbox"}
-				{...register("draft-writing")}
-                className="flex-[14_14_0%]"
+				defaultValue={
+					"Hello world this is the test article and it is long"
+				}
+				{...register("article", { ...ARTICLE_VALIDATION })}
+				className="flex-[14_14_0%]"
 			></textarea>
 		</form>
 	);
