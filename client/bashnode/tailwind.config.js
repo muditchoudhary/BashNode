2562021d8bf45244265627f0261987270624a88e
Mudit Choudhary <cmudit60@gmail.com>
2023-09-07 14:47:37 +0530

    fix tailwind.config.js formating

--- client/bashnode/tailwind.config.js
+++ client/bashnode/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    corePlugins: {
-        preflight: false,
-    },
+	corePlugins: {
+		preflight: false,
+	},
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
@@ -10,15 +10,15 @@ export default {
 				"electric-blue": "#2463EA",
 				"midnight-slate": "#2E353A",
 				"silver-gray": "#BEC1C3",
-                "serene-sky": "#e8edf3",
+				"serene-sky": "#e8edf3",
 			},
 			flex: {
-				"3": "3 3 0%",
-                "2": "2 2 0%",
+				3: "3 3 0%",
+				2: "2 2 0%",
 			},
-            screens: {
-                "desktop": "1440px"
-            }
+			screens: {
+				desktop: "1440px",
+			},
 		},
 	},
 	plugins: [],
