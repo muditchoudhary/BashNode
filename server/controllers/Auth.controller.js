4330217ec5016b384e3369a017e58e8f8561d8e9
Mudit Choudhary <cmudit60@gmail.com>
2023-09-11 15:28:45 +0530

    backend: add model as dependency and refactor the controller
    - By refactoring mean correct the response status code
    - Some code clearning

--- server/controllers/Auth.controller.js
+++ server/controllers/Auth.controller.js
@@ -7,18 +7,18 @@ import { UserModel } from "../models/User.model.js";
 
 dotenv.config();
 
-export const AuthController = () => {
+export const AuthController = (userModel = UserModel) => {
 	const handleSignUp = async (req, res) => {
 		try {
 			const validationErrors = validationResult(req);
 			if (!validationErrors.isEmpty()) {
 				return res
-					.status(400)
+					.status(409)
 					.json({ validationErrors: validationErrors.mapped() });
 			}
 
 			const data = matchedData(req);
-			const user = new UserModel({
+			const user = new userModel({
 				username: data.username,
 				email: data.email,
 				password: await bcrypt.hash(data.password, 10),
@@ -29,9 +29,10 @@ export const AuthController = () => {
 			const token = jwt.sign(payload, process.env.JWT_SECRET, {
 				expiresIn: "1h",
 			});
-			return res.status(200).json({ email: result.email, token });
+			return res.status(200).json({ token });
 		} catch (error) {
 			console.error("Error in handleSignUp:\n\n", error);
+			res.status(500).json({ message: "Internal Server errors" });
 		}
 	};
 
@@ -40,14 +41,14 @@ export const AuthController = () => {
 			const validationErrors = validationResult(req);
 			if (!validationErrors.isEmpty()) {
 				return res
-					.status(400)
+					.status(409)
 					.json({ validationErrors: validationErrors.mapped() });
 			}
 			const data = matchedData(req);
-			const user = await UserModel.findOne({ email: data.email });
+			const user = await userModel.findOne({ email: data.email });
 
 			if (!user) {
-				return res.status(401).json({
+				return res.status(409).json({
 					validationErrors: {
 						email: {
 							msg: "No user found with this email",
@@ -62,7 +63,7 @@ export const AuthController = () => {
 			);
 
 			if (!isPasswordMatch) {
-				return res.status(401).json({
+				return res.status(409).json({
 					validationErrors: {
 						password: {
 							msg: "Password is incorrect",
@@ -75,15 +76,16 @@ export const AuthController = () => {
 			const token = jwt.sign(payload, process.env.JWT_SECRET, {
 				expiresIn: "1h",
 			});
-			return res.status(200).json({ email: user.email, token });
+			return res.status(200).json({ token });
 		} catch (error) {
 			console.error("Error in handleSignIn:\n\n", error);
+            res.status(500).json({ message: "Internal Server errors" });
 		}
 	};
 
 	const authenticateUser = async (jwt_payload, done) => {
 		try {
-			const user = await UserModel.findOne({ _id: jwt_payload.id });
+			const user = await userModel.findOne({ _id: jwt_payload.id });
 			if (!user) {
 				return done(null, false, { message: "User not found" });
 			}
