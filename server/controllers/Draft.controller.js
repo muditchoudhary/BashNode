bd1d977e9c8a2eb5b72338b0f2d8d6d9022ecfff
Mudit Choudhary <cmudit60@gmail.com>
2023-09-11 15:22:43 +0530

    backend: create controllers for draft routes
    - Made saveDraft controller  to to save the draft content to the db.
    - Made the gitTitleAndKeys controller to get the title and keys of all
    drafts and blogs with current sign in user.

--- server/controllers/Draft.controller.js
+++ server/controllers/Draft.controller.js
@@ -1,53 +1,64 @@
-import { UserModel } from "../models/User.model.js";
+import { validationResult, matchedData } from "express-validator";
+
 import { DraftModel } from "../models/Draft.model.js";
 import { PublishedBlogModel } from "../models/Published.model.js";
 
 export const DraftController = (
-	userModel = UserModel,
 	draftModel = DraftModel,
 	publishedBlogModel = PublishedBlogModel
 ) => {
-	const createNewDraft = async (req, res, next) => {
+	const saveDraft = async (req, res, user) => {
 		try {
-			const draft = new userModel();
-			const result = await draft.save();
-			console.log("draft", draft);
-			console.log("result", result);
-			// return res.status(200).json()
+			const validationErrors = validationResult(req);
+			if (!validationErrors.isEmpty()) {
+				return res
+					.status(409)
+					.json({ validationErrors: validationErrors.mapped() });
+			}
+
+			const data = matchedData(req);
+
+			const result = await draftModel.updateOne(
+				{ user_id: user._id },
+				{
+					title: data.title,
+					content: data.content,
+					created_at: Date.now(),
+					updated_at: Date.now(),
+				}
+			);
+
+			return res
+				.status(200)
+				.json({ success: true, message: "Draft saved" });
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
-	const getTitlesAndKeys = async (req, res, next) => {
-		const { email } = JSON.parse(req.headers.user);
+	const getTitlesAndKeys = async (req, res, user) => {
 		try {
-			const user = await userModel.findOne({ email });
-			const userId = user._id;
-
-			let drafts = await draftModel
-				.find({ user_id: userId })
-				.select({ title: 1 });
+			let drafts = await draftModel.find({ user_id: user._id });
 
 			if (drafts.length === 0) {
 				let draft = new draftModel({
-					user_id: userId,
+					user_id: user._id,
 				});
-				await draft.save();
+				const result = await draft.save();
 
-				drafts = await draftModel.find({ user_id: userId });
+				drafts = result;
 			}
 
 			const publishedBlogs = await publishedBlogModel
 				.find({
-					user_id: userId,
+					user_id: user._id,
 				})
 				.select({ title: 1 });
 
 			return res.status(200).json({ drafts, publishedBlogs });
 		} catch (error) {
-			console.error(error);
+			console.error("Error in getTitlesAndKeys:\n\n", err);
 		}
 	};
-	return { getTitlesAndKeys };
+	return { getTitlesAndKeys, saveDraft };
 };
