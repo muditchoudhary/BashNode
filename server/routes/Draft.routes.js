1c08ab154f543e8c2cddde44efcd775427aac86e
Mudit Choudhary <cmudit60@gmail.com>
2023-09-11 15:16:27 +0530

    backend: create a routes for draft feature
    - Made /draft route to get titles and id of all drafts and publised blogs
    - Made /draft/save to save the drave when user press save button

--- server/routes/Draft.routes.js
+++ server/routes/Draft.routes.js
@@ -1,31 +1,33 @@
 import express from "express";
+import passport from "passport";
 
+import { draftValidation } from "../validators/Draft.validation.js";
 import { DraftController } from "../controllers/Draft.controller.js";
 
-export const loadDraftRoutes = (controller = DraftController) => {
+export const loadDraftRoutes = (
+	controller = DraftController,
+	validator = draftValidation
+) => {
 	const router = express.Router();
-	const { getTitlesAndKeys } = controller();
+	const { getTitlesAndKeys, saveDraft } = controller();
+	const { saveDraftValidate } = validator();
 
-	router.get("/draft", getTitlesAndKeys);
+	router.get(
+		"/draft",
+		passport.authenticate("jwt", { session: false }),
+		(req, res) => getTitlesAndKeys(req, res, req.user)
+	);
+	router.post(
+		"/draft/save",
+		passport.authenticate("jwt", { session: false }),
+		saveDraftValidate(),
+		(req, res) => saveDraft(req, res, req.user)
+	);
+	router.post("/draft/test", saveDraftValidate(), (req, res, next) => {
+		const { email } = JSON.parse(req.headers.user);
+		console.log(email);
+		return res.status(200).json(req.headers.user);
+	});
 
 	return router;
 };
-
-// router.post("/publish-blog", async (req, res) => {
-// 	const { email } = JSON.parse(req.headers.user);
-// 	const { title, content } = req.body.draft;
-// 	const user = await UserModel.findOne({ email });
-// 	const userId = user ? user._id : null;
-
-// 	const newBlog = new PublishedBlogModel({
-// 		title: title,
-// 		content: content,
-// 		user_id: userId,
-// 		created_at: Date.now(),
-// 		updated_at: Date.now(),
-// 	});
-
-// 	await newBlog.save();
-
-// 	res.status(200).json({ message: "success" });
-// });
