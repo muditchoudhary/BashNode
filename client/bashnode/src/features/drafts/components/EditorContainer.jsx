9055d8fb0842c678d9284f95720bb020893b722e
Mudit Choudhary <cmudit60@gmail.com>
2023-10-17 11:28:35 +0530

    Update props

--- client/bashnode/src/features/drafts/components/EditorContainer.jsx
+++ client/bashnode/src/features/drafts/components/EditorContainer.jsx
@@ -3,14 +3,27 @@ import PropTypes from "prop-types";
 import { ActionBar } from "./Actionbar";
 import { TextEditor } from "./TextEditor";
 
-export const EditorContainer = ({ currentBlog }) => {
+export const EditorContainer = ({
+	currentBlog,
+	isDraftWindow,
+	setCurrentDraft,
+	setCurrentPublished,
+}) => {
 	return (
 		<div className="editor-container gap-2 p-2 borer-2 flex flex-col flex-1">
 			<ActionBar />
-			<TextEditor currentBlog={currentBlog} />
+			<TextEditor
+				currentBlog={currentBlog}
+				isDraftWindow={isDraftWindow}
+				setCurrentDraft={setCurrentDraft}
+                setCurrentPublished={setCurrentPublished}
+			/>
 		</div>
 	);
 };
 EditorContainer.propTypes = {
 	currentBlog: PropTypes.object.isRequired,
+	isDraftWindow: PropTypes.bool.isRequired,
+	setCurrentDraft: PropTypes.func,
+	setCurrentPublished: PropTypes.func,
 };
