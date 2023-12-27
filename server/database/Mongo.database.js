5b493852859a326e43a34b1bf6c8d05716b3114b
Mudit Choudhary <cmudit60@gmail.com>
2023-12-27 16:43:01 +0530

    Add build script

--- server/database/Mongo.database.js
+++ server/database/Mongo.database.js
@@ -7,17 +7,26 @@ const BLOGSDB_URI = process.env.BLOGSDB_URI;
 const USERSDB_URI = process.env.USERSDB_URI;
 
 export const connectDBs = () => {
-	try {
-		const usersDB = mongoose.createConnection(USERSDB_URI, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
-		const blogsDB = mongoose.createConnection(BLOGSDB_URI, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-		});
-		return { usersDB, blogsDB };
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-	}
+    try {
+        const usersDB = mongoose.createConnection(USERSDB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        const blogsDB = mongoose.createConnection(BLOGSDB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+
+        usersDB.on("connected", () => {
+            console.log("UsersDB connection successful");
+        });
+
+        blogsDB.on("connected", () => {
+            console.log("BlogsDB connection successful");
+        });
+
+        return { usersDB, blogsDB };
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
 };
